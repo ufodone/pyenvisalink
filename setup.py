32a72b27abe5bc927747a7c60a5c9d14ed7502fd
Dan Cinnamon <dcinnamon@gmail.com>
2016-05-15 01:06:25 -0500

    Created tag 0.1 and preparing for submission

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='pyenvisalink',
       version='0.1',
       description='A python3 library for running asynchronus communications with envisalink alarm control panel modules.',
       url='https://github.com/Cinntax/pyenvisalink',
+      download_url='https://github.com/Cinntax/pyenvisalink/tarball/0.1'
       author='Dan Cinnamon',
       author_email='dcinnamon@gmail.com',
       license='MIT',
