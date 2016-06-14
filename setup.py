d23a3a2d2cccd8d4c1876c833befda8e42e69677
Dan Cinnamon <dcinnamon@gmail.com>
2016-06-14 00:09:40 -0500

    release 0.7

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(name='pyenvisalink',
-      version='0.6',
+      version='0.7',
       description='A python3 library for running asynchronus communications with envisalink alarm control panel modules.',
       url='https://github.com/Cinntax/pyenvisalink',
-      download_url='https://github.com/Cinntax/pyenvisalink/tarball/0.6',
+      download_url='https://github.com/Cinntax/pyenvisalink/tarball/0.7',
       author='Dan Cinnamon',
       author_email='dcinnamon@gmail.com',
       license='MIT',
