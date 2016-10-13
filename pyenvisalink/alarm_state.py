fb211cd4caf64c1a1c40eee620b9a64cefefab04
jnimmo <james@nimmo.net.nz>
2016-10-13 22:29:37 +1300

    Update alarm_state.py

--- pyenvisalink/alarm_state.py
+++ pyenvisalink/alarm_state.py
@@ -13,7 +13,7 @@ class AlarmState:
                                                       'armed_zero_entry_delay': False, 'alarm_fire_zone': False,
                                                       'trouble': False, 'bat_trouble': False, 'ready': False, 'fire': False,
                                                       'armed_stay': False, 'alpha': 'N/A', 'beep': False,
-                                                      'exit_delay': False, 'entry_delay': False, 'last_changed_by_user': 'N/A'}}
+                                                      'exit_delay': False, 'entry_delay': False, 'last_changed_by_user': ''}}
         for j in range (1, maxZones + 1):
             _alarmState['zone'][j] = {'status': {'open': False, 'fault': False, 'alarm': False, 'tamper': False}, 'last_fault': 0}
 
