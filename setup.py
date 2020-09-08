949026cd689d4026e4aa7e41a3d3c2a7a11f54da
cinntax <dcinnamon@gmail.com>
2020-09-07 23:20:22 -0500

    Roll out v4.1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(name='pyenvisalink',
-      version='4.0',
+      version='4.1',
       description='A python3 library for running asynchronus communications with envisalink alarm control panel modules.',
       url='https://github.com/Cinntax/pyenvisalink',
-      download_url='https://github.com/Cinntax/pyenvisalink/tarball/4.0',
+      download_url='https://github.com/Cinntax/pyenvisalink/tarball/4.1',
       author='Dan Cinnamon',
       author_email='dcinnamon@gmail.com',
       license='MIT',
