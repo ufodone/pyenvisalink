d73782991a2b9eed607fd836065e9a833a93d766
RichieFrame <33644730+RichieFrame@users.noreply.github.com>
2021-08-03 03:09:00 -0500

    Fixes and additions to state array
    Renames armed_bypass to bypass since this is true even when not armed. Breaking change. Also adds an entry for the internal partition state for Honeywell panels to report the true status, as HomeAssistant does not currently support the same arming modes as the panel, namely INSTANT

--- pyenvisalink/alarm_state.py
+++ pyenvisalink/alarm_state.py
@@ -8,9 +8,11 @@ class AlarmState:
         _alarmState = {'partition': {}, 'zone': {}}
 
         for i in range(1, maxPartitions + 1):
-            _alarmState['partition'][i] = {'status': {'alpha': 'N/A', 
+            _alarmState['partition'][i] = {'status': {'partition_state': 'N/A', 
+                                                      'alpha': 'N/A', 
                                                       'ac_present': True, 
                                                       'beep': False, 
+                                                      'bypass': False, 
                                                       'chime': False, 
                                                       'entry_delay': False, 
                                                       'exit_delay': False, 
@@ -24,7 +26,6 @@ class AlarmState:
                                                       'alarm_fire_zone': False, 
                                                       'alarm_in_memory': False, 
                                                       'armed_away': False, 
-                                                      'armed_bypass': False, 
                                                       'armed_stay': False, 
                                                       'armed_zero_entry_delay': False }}
         for j in range (1, maxZones + 1):
