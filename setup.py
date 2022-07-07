25fdc0ab6a6775e676e148d44c50759d91f6317c
David O'Neill <ufodone@gmail.com>
2022-07-06 17:57:00 -0700

    Release version 4.6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(name='pyenvisalink',
-      version='4.5',
+      version='4.7',
       description='A python3 library for running asynchronus communications with envisalink alarm control panel modules.',
       url='https://github.com/Cinntax/pyenvisalink',
-      download_url='https://github.com/Cinntax/pyenvisalink/tarball/4.5',
-      author='Dan Cinnamon',
-      author_email='dcinnamon@gmail.com',
+      download_url='https://github.com/ufodone/pyenvisalink/tarball/4.6',
+      author="David O'Neill",
+      author_email='ufodone@gmail.com',
       license='MIT',
       packages=['pyenvisalink'],
-      classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.5', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7']
+      classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.9', 'Programming Language :: Python :: 3.10']
       )
