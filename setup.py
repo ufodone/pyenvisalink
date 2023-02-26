d53fbc5b2f9b086c7592bba47379b056b5e5d9d8
David O'Neill <ufodone@gmail.com>
2023-02-26 15:07:16 -0800

    Added long_description as needed by PyPi

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
         "A python3 library for running asynchronus communications with envisalink "
         "alarm control panel modules."
     ),
+    long_description=(
+        "A python3 library for running asynchronus communications with envisalink "
+        "alarm control panel modules."
+    ),
     url="https://github.com/ufodone/pyenvisalink",
     author="David O'Neill",
     author_email="ufodone@gmail.com",
