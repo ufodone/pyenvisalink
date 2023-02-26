6667538cf9c00150545d158a6ee6e4200142cb5f
David O'Neill <ufodone@gmail.com>
2023-02-26 14:55:32 -0800

    Bumped version to 5.0.0b1

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup
 
 setup(
     name="pyenvisalink",
-    version="4.6",
+    version="5.0.0b1",
     description=(
         "A python3 library for running asynchronus communications with envisalink "
         "alarm control panel modules."
     ),
-    url="https://github.com/Cinntax/pyenvisalink",
-    download_url="https://github.com/ufodone/pyenvisalink/tarball/4.6",
+    url="https://github.com/ufodone/pyenvisalink",
     author="David O'Neill",
     author_email="ufodone@gmail.com",
     license="MIT",
     packages=["pyenvisalink"],
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
 )
